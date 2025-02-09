2c132747228d5066a0e93dfe5faddb712f3089f0
Илья Голод <67516960+1lyagolod@users.noreply.github.com>
2025-02-09 19:13:45 +0300

    Delete back/src/main/java/com/app/policy directory

--- back/src/main/java/com/app/policy/PolicyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.policy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
